2008cae0a38169d9527372f88284f62118e0541c
Kerry Snyder <kerry.snyder@gmail.com>
2015-04-09 10:38:36 -0400

    Revert "Add tons of efficiency improvements, hopefully increasing perf with better accuracy"
    This reverts commit c4f3f7f222fb3ab6cc820d71e2bd5ee4a1a154e7.

--- raspi/kinect_handler/crowd_detector.h
+++ raspi/kinect_handler/crowd_detector.h
@@ -3,7 +3,7 @@
 class CrowdDetector {
 	public:
 		CrowdDetector(int min_faces, int max_depth, int scale);
-		bool detectCrowd(const cv::Mat rgb, cv::Point* centroid, std::vector<cv::Point>& ff);
+		bool detectCrowd(const cv::Mat& depth, const cv::Mat rgb, cv::Point* centroid, std::vector<cv::Point>& ff);
 	private:
 		int min_faces;
 		int max_depth;
--- raspi/kinect_handler/my_freenect_device.cpp
+++ raspi/kinect_handler/my_freenect_device.cpp
@@ -1,105 +1,59 @@
 #include "my_freenect_device.h"
-#include <iostream>
-#include <vector>
-#include <cstdio>
-
-#define SCALE 4
-//#define MIN_FACES 1
-//#define MAX_DEPTH 123
-//#define DIS 1
-#define WIDTH 640
-#define HEIGHT 480
-#define SWIDTH (WIDTH/SCALE)
-#define SHEIGHT (HEIGHT/SCALE)
-
-#define RESCALE_X(xs) (((xs - (SWIDTH/2))*SCALE)+(WIDTH/2))
-#define RESCALE_Y(ys) (((ys - (SHEIGHT/2))*SCALE)+(HEIGHT/2))
 
 MyFreenectDevice::MyFreenectDevice(freenect_context *_ctx, int _index)
-    : Freenect::FreenectDevice(_ctx, _index),
-    depthMat(cv::Size(WIDTH,HEIGHT),CV_16UC1, cv::Scalar(0)),
-    rgbMat(cv::Size(WIDTH,HEIGHT), CV_8UC3, cv::Scalar(0)),
-    rgbMat_s(cv::Size(SWIDTH,SHEIGHT), CV_8UC3, cv::Scalar(0)) {
-	K = cv::Mat::zeros(3, 3, CV_64F);
-	K.at<double>(0,0) = 517.05;
-	K.at<double>(1,1) = 517.02;
-	K.at<double>(0,2) = 324.18;
-	K.at<double>(1,2) = 262.17;
-	K.at<double>(2,2) = 1.0;
-	cv::Mat K1 = cv::Mat::zeros(3, 3, CV_64F);
-	cv::Mat K2 = cv::Mat::zeros(3, 3, CV_64F);
-	cv::Mat K3 = cv::Mat::zeros(3, 3, CV_64F);
-	K1.at<double>(0,0) = 1;
-	K1.at<double>(1,1) = 1;
-	K1.at<double>(0,2) = -(SWIDTH/2);
-	K1.at<double>(1,2) = -(SHEIGHT/2);
-	K1.at<double>(2,2) = 1.0;
-	K2.at<double>(0,0) = SCALE;
-	K2.at<double>(1,1) = SCALE;
-	K2.at<double>(2,2) = 1.0;
-	K3.at<double>(0,0) = 1;
-	K3.at<double>(1,1) = 1;
-	K3.at<double>(0,2) = 320;
-	K3.at<double>(1,2) = 240;
-	K3.at<double>(2,2) = 1.0;
-	resize = cv::Mat::zeros(3, 3, CV_64F);
-	resize = K3*K2*K1;
-	tracker = new BlobTracker(SCALE);
+	: Freenect::FreenectDevice(_ctx, _index), m_buffer_depth(FREENECT_DEPTH_11BIT),
+	m_buffer_rgb(FREENECT_VIDEO_RGB), m_gamma(2048), m_new_rgb_frame(false),
+	m_new_depth_frame(false), depthMat(cv::Size(640,480),CV_16UC1),
+	rgbMat(cv::Size(640,480), CV_8UC3, cv::Scalar(0)),
+	ownMat(cv::Size(640,480),CV_8UC3,cv::Scalar(0)) {
 
-	pthread_mutex_init(&leader_pos_lock, NULL);
-    }
+		for( unsigned int i = 0 ; i < 2048 ; i++) {
+			float v = i/2048.0;
+			v = std::pow(v, 3)* 6;
+			m_gamma[i] = v*6*256;
+		}
+	}
 
-void MyFreenectDevice::start(void) {
-    startVideo();
-    startDepth();
-}
 // Do not call directly even in child
 void MyFreenectDevice::VideoCallback(void* _rgb, uint32_t timestamp) {
-    uint8_t* rgb = static_cast<uint8_t*>(_rgb);
-    rgbMat.data = rgb;
-    cv::pyrDown(rgbMat,rgbMat_s, cv::Size(SWIDTH,SHEIGHT));
-    cv::Point blob_centroid;
-    bool predicted;
-    cv::Mat mask;
-    if (tracker->detect_leader(rgbMat_s, mask, &blob_centroid)) {
-	Eigen::Vector2d p(blob_centroid.x, blob_centroid.y);
-	tracker->observe(p);
-	predicted = false;
-    } else {
-	Eigen::Vector2d pred_loc = tracker->predict();
-	blob_centroid.x = pred_loc.x();
-	blob_centroid.y = pred_loc.y();
-	predicted = true;
-    }
-    pthread_mutex_lock(&leader_pos_lock);
-    leader_loc = blob_centroid;
-    pthread_mutex_unlock(&leader_pos_lock);
-    cv::Mat leader_direction(3,1,CV_64F);
-    leader_direction.at<double>(0,0) = blob_centroid.x;
-    leader_direction.at<double>(1,0) = blob_centroid.y;
-    leader_direction.at<double>(2,0) = 1.0;
-    cv::Mat lead_vec = K.inv()*resize*leader_direction;
-    lead_vec = lead_vec/cv::norm(lead_vec);
-    if (predicted) {
-	    std::cout << "P: " << lead_vec.at<double>(0,0) << "," << lead_vec.at<double>(1,0) << "," << lead_vec.at<double>(2,0) << std::endl;
-    } else {
-	    std::cout << "O: " << lead_vec.at<double>(0,0) << "," << lead_vec.at<double>(1,0) << "," << lead_vec.at<double>(2,0) << std::endl;
-    }
+	m_rgb_mutex.lock();
+	uint8_t* rgb = static_cast<uint8_t*>(_rgb);
+	rgbMat.data = rgb;
+	m_new_rgb_frame = true;
+	m_rgb_mutex.unlock();
 }
 
 // Do not call directly even in child
 void MyFreenectDevice::DepthCallback(void* _depth, uint32_t timestamp) {
-    uint16_t* depth = static_cast<uint16_t*>(_depth);
-    depthMat.data = (uchar*) depth;
-    pthread_mutex_lock(&leader_pos_lock);
-    cv::Point blob_pos = leader_loc;
-    pthread_mutex_unlock(&leader_pos_lock);
+	m_depth_mutex.lock();
+	uint16_t* depth = static_cast<uint16_t*>(_depth);
+	depthMat.data = (uchar*) depth;
+	m_new_depth_frame = true;
+	m_depth_mutex.unlock();
+}
 
-    double blob_depth;
-    if (blob_pos.x == 320 && blob_pos.y == 240) {
-	blob_depth = 810;
-    } else {
-	blob_depth = depthMat.at<unsigned short>(RESCALE_Y(blob_pos.y), RESCALE_X(blob_pos.x));
-    }
-    std::cout << "B: " << blob_depth << std::endl;
+bool MyFreenectDevice::getVideo(cv::Mat& output) {
+	m_rgb_mutex.lock();
+	if(m_new_rgb_frame) {
+		cv::cvtColor(rgbMat, output, CV_RGB2BGR);
+		m_new_rgb_frame = false;
+		m_rgb_mutex.unlock();
+		return true;
+	} else {
+		m_rgb_mutex.unlock();
+		return false;
+	}
+}
+
+bool MyFreenectDevice::getDepth(cv::Mat& output) {
+	m_depth_mutex.lock();
+	if(m_new_depth_frame) {
+		depthMat.copyTo(output);
+		m_new_depth_frame = false;
+		m_depth_mutex.unlock();
+		return true;
+	} else {
+		m_depth_mutex.unlock();
+		return false;
+	}
 }
